a66d86c4a8a38ed756ec4712d83e0946f8d9635a
Noah G E [STUDENT] <ne6mq07@limestone.on.ca>
2025-05-15 13:07:03 -0400

    Create GeneralInfo.java

---
+++ app/src/main/java/com/fss/everythingapp/fitness/GeneralInfo.java
@@ -0,0 +1,11 @@
+package com.fss.everythingapp.fitness;
+
+public class GeneralInfo {
+    private String name;
+    private int age;
+    private double height; // in cm
+    private double weight; // in kg
+    private boolean gender;
+    private String activityLevel; 
+
+}
