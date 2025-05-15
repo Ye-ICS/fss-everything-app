c82968c0b4db94d25115f24620482ebdbd6523ff
B1gCD <devlinc@limestone.on.ca>
2025-05-15 13:03:24 -0400

    basic variables/setup
    Focusing on theory and planning

--- app/src/main/java/com/fss/everythingapp/fitness/Diet.java
+++ app/src/main/java/com/fss/everythingapp/fitness/Diet.java
@@ -1,4 +1,11 @@
 package com.fss.everythingapp.fitness;
 public class Diet {
+    double calories;
+    double protein;
+    double carbs;
+    double bodyfat;
+    double fats;
+    double cholesterol;
+
     
 }
