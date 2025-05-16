d6596f12b624890fc403baaeb6c9d310f64eb3e2
Christos857 <Christosidi15@gmail.com>
2025-05-16 13:06:55 -0400

    h

---
+++ app/src/main/java/com/fss/everythingapp/studentServices/CalendarRetrieval.java
@@ -0,0 +1,12 @@
+package com.fss.everythingapp.studentservices;
+
+class CalendarRetrieval {
+    private void getCalendarInfo() {
+        //access councellor's calendar
+    }
+
+    
+    private void sendCalendarInfo() {
+        //update councellor's calendar
+    }
+}
