98050dd4b09c94c9f36c5313b3477cb6a714b664
ShadowJaguar1 <willdelouche3@gmail.com>
2025-05-16 12:34:03 -0400

    fixed classpath

---
+++ app/src/main/java/com/fss/everythingapp/studentServices/StudentServices.java
@@ -0,0 +1,5 @@
+package com.fss.everythingapp.studentServices;
+
+public class StudentServices {
+    
+}
--- app/src/studentServices/StudentServices.java
+++
@@ -1,5 +0,0 @@
-package com.fss.everythingapp.app.studentServices;
-
-public class StudentServices {
-    
-}
