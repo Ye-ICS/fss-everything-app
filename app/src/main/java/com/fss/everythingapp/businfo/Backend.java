49ee6b16d1988287a6956ba787c7fbeea94d9981
base crown <>
2025-05-16 13:06:57 -0400

    initial skeleton

--- app/src/main/java/com/fss/everythingapp/businfo/Backend.java
+++ app/src/main/java/com/fss/everythingapp/businfo/Backend.java
@@ -1,5 +1,13 @@
 package com.fss.everythingapp.businfo;
 
 public class Backend {
+    public RealData updateRealTime(){
+        return new RealData();
+    }
 
+    public StaticData fetchStaticData(){
+        return new StaticData();
+    }
+
+    
 }
