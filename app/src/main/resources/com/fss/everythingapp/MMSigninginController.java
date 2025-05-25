43d49f8d7b5f1770f3ccae92e775aa347869265f
Maher <157660848+MaherHssn@users.noreply.github.com>
2025-05-25 19:36:31 -0400

    Signing in fxml and controller added

---
+++ app/src/main/resources/com/fss/everythingapp/MMSigninginController.java
@@ -0,0 +1,70 @@
+package com.fss.everythingapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class MMSigninginController {
+
+  @FXML
+    private ToggleGroup ActivityChoice;
+
+    @FXML
+    private RadioButton activeRadio;
+
+    @FXML
+    private TextField ageField;
+
+    @FXML
+    private RadioButton femaleRadio;
+
+    @FXML
+    private ToggleGroup genderToggle;
+
+    @FXML
+    private TextField heightField;
+
+    @FXML
+    private RadioButton maleRadio;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private RadioButton notActiveRadio;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private RadioButton veryActiveRadio;
+
+    @FXML
+    private TextField weightField;
+
+
+    @FXML
+    private void handleActivityChoice(ActionEvent event) {
+        RadioButton selectedRadio = (RadioButton) ActivityChoice.getSelectedToggle();
+        String activityLevel = selectedRadio.getText();
+        System.out.println("Selected activity level: " + activityLevel);
+    }
+    @FXML
+    private void handleGenderChoice(ActionEvent event) {
+        RadioButton selectedRadio = (RadioButton) genderToggle.getSelectedToggle();
+    }
+    @FXML  
+    private void handleTextFieldAction(ActionEvent event) {
+        String name = nameField.getText();
+        String age = ageField.getText();
+        String height = heightField.getText();
+        String weight = weightField.getText();
+        boolean isMale = maleRadio.isSelected();
+        boolean isFemale = femaleRadio.isSelected();
+        String activityLevel = ((RadioButton) ActivityChoice.getSelectedToggle()).getText();
+    }
+
+}
--- app/src/main/resources/com/fss/everythingapp/MMnsiController.java
+++ app/src/main/resources/com/fss/everythingapp/MMnsiController.java
@@ -1,8 +1,15 @@
 package com.fss.everythingapp;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MMnsiController {
 
@@ -11,7 +18,16 @@ public class MMnsiController {
 
     @FXML
     void signIn(ActionEvent event) {
-        System.out.println("Sign In button clicked");
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/fss/everythingapp/app/fxml/MainMenu-Signedin.fxml"));
+        Parent signInScreen = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(signInScreen));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
 
 }
