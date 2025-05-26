6b090464fde8d79fdf1637367512de9e63107fc4
Declan Jones <pm@declanjones.ca>
2025-05-26 12:12:43 -0400

    Add initial FXML layout for bus information UI and update Interface class

--- app/src/main/java/com/fss/everythingapp/businfo/MainListController.java
+++ app/src/main/java/com/fss/everythingapp/businfo/MainListController.java
@@ -1,20 +1,6 @@
 package com.fss.everythingapp.businfo;
 
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.layout.BorderPane;
 
 public class MainListController {
-    
-    @FXML
-    private BorderPane rootContainer;
 
-    @FXML
-    private void search(ActionEvent event) {
-        //search();
-    }
 }
