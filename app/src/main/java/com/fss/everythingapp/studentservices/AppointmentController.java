505815a1f1cfb5a19c44faa450b63e67d287c300
ShadowJaguar1 <willdelouche3@gmail.com>
2025-05-26 12:52:03 -0400

    filled controller method

--- app/src/main/java/com/fss/everythingapp/studentservices/AppointmentController.java
+++ app/src/main/java/com/fss/everythingapp/studentservices/AppointmentController.java
@@ -1,12 +1,26 @@
 package com.fss.everythingapp.studentservices;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
 public class AppointmentController {
     static String time;
     static String name;
     static int grade;
+    private BorderPane rootcontainer;
 
-    private void enterBtnPressed() {
-        
+    @FXML
+    private void enterBtnPressed() throws IOException {
+        // aquire name and grade
+
+
+        // load to the next layout
+        Parent timeslotsLayout = FXMLLoader.load(getClass().getResource("Timeslots.fxml"));
+        rootcontainer.getScene().setRoot(timeslotsLayout);
     }
 
 }
