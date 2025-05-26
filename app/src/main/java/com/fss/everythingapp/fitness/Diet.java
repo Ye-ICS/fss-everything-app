83353cf1143ac9bd846097c8203d92f30d63e4b8
Noah G E [STUDENT] <ne6mq07@limestone.on.ca>
2025-05-26 12:22:53 -0400

    cleaned up methods and rearranged

--- app/src/main/java/com/fss/everythingapp/fitness/Diet.java
+++ app/src/main/java/com/fss/everythingapp/fitness/Diet.java
@@ -1,6 +1,7 @@
 package com.fss.everythingapp.fitness;
 public class Diet {
     static protected double calories;
+    static protected double calBurned;
     private double protein;
     private double carbs;
     private double fats;
--- app/src/main/java/com/fss/everythingapp/fitness/GeneralInfo.java
+++ app/src/main/java/com/fss/everythingapp/fitness/GeneralInfo.java
@@ -3,25 +3,18 @@ package com.fss.everythingapp.fitness;
 public class GeneralInfo {
     private String name;
     private int age;
-    private double height; // in cm
+    static protected double height; // in cm
     static protected double weight; // in kg
     private double bodyfat; // %
-    private boolean gender; // true is male, female is false
+    private boolean isMale;
+    private boolean isFemale; // true is male, female is false
     //static protected boolean activityLevel; // not active, active, super active
-    static private boolean skinnyBuild;
-    static private boolean averageBuild;
-    static private boolean fitBuild;
-    static private boolean bigBuild;
     private boolean athleteFat;
     private boolean acceptableFat;
-    static private boolean underweight;
-    static private boolean healthyweight;
-    static private boolean overweight;
     static private boolean obesityFat;
-    private double bmiIndex;
 
     boolean isActive () {
-        if (Diet.calories < 1600) {
+        if (Diet.calBurned < 1600) {
             return false;
         } else {
             return true;
@@ -29,7 +22,7 @@ public class GeneralInfo {
     }
 
     void calculateBodyfat () {
-        if (gender == true) {
+        if (isMale == true) {
             
              if (bodyfat > 5 && bodyfat < 13.1) {
                 athleteFat = true;
@@ -49,72 +42,6 @@ public class GeneralInfo {
         }
     }
 
-    void calculateBmiAndWeight () {
-        bmiIndex = (weight) / (Math.sqrt((height / 100)));
-        if (bmiIndex < 18.5) {
-            underweight = true;
-        } else if (bmiIndex > 18.4 && bmiIndex < 25) {
-            healthyweight = true;
-        } else {
-            overweight = true;        
-        } 
-    }
-    
-    void calculatePhysique () {
-        if (gender == true) {
-            if (underweight == true && athleteFat == true) {
-                fitBuild = true;
-            } else if (underweight == true && acceptableFat) {
-                averageBuild = true;
-            } else if (underweight == true && obesityFat) {
-                skinnyBuild = true;
-            } else if (healthyweight && athleteFat) {
-                fitBuild = true;
-            } else if (healthyweight && acceptableFat) {
-                averageBuild = true;
-            } else if (healthyweight && obesityFat) {
-                averageBuild = true;
-            } else if (overweight && athleteFat) {
-                fitBuild = true;
-            } else if (overweight && acceptableFat) {
-                bigBuild = true;
-            } else if (overweight && obesityFat) {
-                bigBuild = true;
-            }
-        }
-    }
-
-
-    static boolean getSkinnyBuild () {
-       if (skinnyBuild == true) {
-        return true;
-       } else {
-        return false;
-       }
-    }
-
-    static boolean getAverageBuild () {
-        if (averageBuild == true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    static boolean getFitBuild () {
-        if (fitBuild == true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    static boolean getBigBuild () {
-        if (bigBuild == true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+   
 
 }
