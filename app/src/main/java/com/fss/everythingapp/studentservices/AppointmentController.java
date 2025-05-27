18c67194240e6b5dd8d3c27f5d336fe7ba4708a1
ShadowJaguar1 <willdelouche3@gmail.com>
2025-05-27 12:46:55 -0400

    restructured Appointmentlayout

--- app/src/main/java/com/fss/everythingapp/studentservices/AppointmentController.java
+++ app/src/main/java/com/fss/everythingapp/studentservices/AppointmentController.java
@@ -1,7 +1,6 @@
 package com.fss.everythingapp.studentservices;
 
 import java.io.IOException;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
