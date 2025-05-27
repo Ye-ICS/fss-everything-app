75053f2f508d21ee29f030961d2de63dd2a45c6d
Declan Jones <pm@declanjones.ca>
2025-05-27 12:29:56 -0400

    Implement bus navigation and update UI components
    - Added openBuses method in MainMenuController to load bus information UI.
    - Updated Example.fxml to link the Buses button to the new method.
    - Introduced quit method in MainListController for exit functionality.
    - Modified List.fxml to connect the exit button to the quit method.
    - Removed unused Interface class.

--- app/src/main/java/com/fss/everythingapp/businfo/MainListController.java
+++ app/src/main/java/com/fss/everythingapp/businfo/MainListController.java
@@ -1,6 +1,22 @@
 package com.fss.everythingapp.businfo;
 
+import java.io.IOException;
+
+import com.fss.everythingapp.app.App;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 
 public class MainListController {
 
+    @FXML
+    BorderPane rootContainer;
+
+    @FXML
+    private void quit(ActionEvent actionEvent) throws IOException {
+        
+    }
 }
