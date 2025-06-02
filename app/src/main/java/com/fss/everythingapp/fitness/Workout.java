d826e083c58213ac6962c96d19a0c990ea355869
NoahE321 <ne6mq07@limestone.on.ca>
2025-06-02 12:06:51 -0400

    Update Workout.java

--- app/src/main/java/com/fss/everythingapp/fitness/Workout.java
+++ app/src/main/java/com/fss/everythingapp/fitness/Workout.java
@@ -1,6 +1,6 @@
 package com.fss.everythingapp.fitness;
 public class Workout {
     private boolean worksOut;
-    static  protected String desiredPhysique; // skinny, lean, bulk, 
+    static  public String desiredPhysique; // skinny, lean, bulk, 
     
 }
