b548afe37e7d607564a2284f2bc64b8a4fb4ec18
ShadowJaguar1 <willdelouche3@gmail.com>
2025-06-02 13:00:44 -0400

    Added new classes to help create the fake API

--- app/src/main/java/com/fss/everythingapp/studentservices/Date.java
+++ app/src/main/java/com/fss/everythingapp/studentservices/Date.java
@@ -1,8 +1,9 @@
 package com.fss.everythingapp.studentservices;
 
-class Date  {
-    int[] times = new int[7];
+import java.util.ArrayList;
+import java.util.List;
 
+<<<<<<< Updated upstream
     void Date(int[] times) {
         this.times = times;
     }
@@ -10,4 +11,16 @@ class Date  {
     int[] getTimes() {return this.times;}
 
     
+=======
+class Date  {
+    List<Timeslot> times = new ArrayList<Timeslot>();
+
+    //TODO fill arraylist with timeslot instances,give each timeslot a designation for the time it represents
+
+    Date(List<Timeslot> times) {
+        this.times = times;
+    }
+
+    List<Timeslot> getTimes() {return this.times;}
+>>>>>>> Stashed changes
 }
