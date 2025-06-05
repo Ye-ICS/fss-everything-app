1b41d83b1b5721f2eee8bef4b40658458b56b2f4
NoahE321 <ne6mq07@limestone.on.ca>
2025-06-05 12:56:13 -0400

    Update SleepTimer.java

--- app/src/main/java/com/fss/everythingapp/fitness/SleepTimer.java
+++ app/src/main/java/com/fss/everythingapp/fitness/SleepTimer.java
@@ -1,23 +1,43 @@
 package com.fss.everythingapp.fitness;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SleepTimer {
-    
-    void ButtonStart() {
-        // Start the sleep timer
+
+     public static void main(String[] args) {
+      LocalDateTime myDateObj = LocalDateTime.now();
+    System.out.println("Before formatting: " + myDateObj);
+    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    String formattedDate = myDateObj.format(myFormatObj);
+    System.out.println("After formatting: " + formattedDate);
     }
 
-    void ButtonStop() {
+    void ButtonStart(LocalDateTime myDateObj) {
+        // Start the sleep timer
+        SleepSchedule.slept = myDateObj.getHour();
+    }
+
+    void ButtonStop(LocalDateTime myDateObj) {
         // Stop the sleep timer
+    SleepSchedule.woke = myDateObj.getHour();
+    SleepSchedule.timeslept = SleepSchedule.slept - SleepSchedule.woke;
+    if (SleepSchedule.timeslept < 0) {
+        SleepSchedule.timeslept = SleepSchedule.timeslept + 24;
+    }
     }
 
     void ButtonReset() {
         // Reset the sleep timer
+        SleepSchedule.slept = 0;
+        SleepSchedule.woke = 0;
+        SleepSchedule.timeslept = 0;
     }
 
     void ButtonSave() {
         // Save the sleep timer data
+        
     }
 
     void ButtonLoad() {
