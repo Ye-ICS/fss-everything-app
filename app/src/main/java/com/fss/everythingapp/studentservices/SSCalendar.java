bb17ebee7c471913567211f9333bbbbf227e8934
ShadowJaguar1 <willdelouche3@gmail.com>
2025-06-06 12:49:20 -0400

    synchronized code

--- app/src/main/java/com/fss/everythingapp/studentservices/SSCalendar.java
+++ app/src/main/java/com/fss/everythingapp/studentservices/SSCalendar.java
@@ -2,8 +2,6 @@ package com.fss.everythingapp.studentservices;
 
 import java.util.ArrayList;
 
-//TODO feed SScalendar, Date, and Timeslot into an AI and aske to have a mock API class with the desired method.
-
 import java.util.List;
 
 public class SSCalendar {
@@ -13,9 +11,6 @@ public class SSCalendar {
     SSCalendar(Long id, List<Date> dates) {
         this.dates = dates;
     }
-
-    // TODO fill arraylist with date instances, give each date a designation for the
-    // date it represents
 
     public List<Date> getAvailableDates() {
         return dates;
