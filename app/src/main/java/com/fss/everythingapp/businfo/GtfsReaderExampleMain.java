1a3bd8f655228d09833a65821613aca209de9093
base crown <>
2025-06-09 12:17:10 -0400

    Make busses accessible by ID.

--- app/src/main/java/com/fss/everythingapp/businfo/GtfsReaderExampleMain.java
+++ app/src/main/java/com/fss/everythingapp/businfo/GtfsReaderExampleMain.java
@@ -6,17 +6,18 @@ import com.google.transit.realtime.GtfsRealtime.FeedEntity;
 import com.google.transit.realtime.GtfsRealtime.FeedMessage;
 
 public class GtfsReaderExampleMain {
-  public GtfsReaderExampleMain() throws Exception {
-    System.out.println("test1");
+  public GtfsReaderExampleMain(String id) throws Exception {
+    
     URL url = new URL("https://api.cityofkingston.ca/gtfs-realtime/vehicleupdates.pb");
     FeedMessage feed = FeedMessage.parseFrom(url.openStream());
     System.out.println("test2");
+    
     for (FeedEntity entity : feed.getEntityList()) {
-      if (entity.hasVehicle()) {
+      if (entity.hasVehicle() && entity.getVehicle().getVehicle().getId().equals("3490100053")) {
       System.out.println("Vehicle ID: " + entity.getVehicle().getVehicle().getId());
       System.out.println("Position: " + entity.getVehicle().getPosition());
       }
     }
-    System.out.println("done");
+   
   }
 }
